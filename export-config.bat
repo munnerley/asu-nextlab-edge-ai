37abd9d40dbad797b4d66d915f9b2a555cd6395a
Dan Munnerley <dan@munnerley.com>
2026-03-22 20:14:11 -0700

    merge Lovable build into export-config

--- export-config.bat
+++ export-config.bat
@@ -2,8 +2,12 @@
 echo Exporting ASU Next Lab Edge AI Config...
 echo.
 
+set DEMODIR=%~dp0
+set DEMODIR=%DEMODIR:~0,-1%
+set LOVABLEDIR=C:\Users\dan\Documents\NextLab\asu-next-lab-edge-ai
+
 REM Get current version
-for /f "usebackq delims=" %%a in ("version.txt") do set CURRENT_VERSION=%%a
+for /f "usebackq delims=" %%a in ("%DEMODIR%\version.txt") do set CURRENT_VERSION=%%a
 echo Current version: v%CURRENT_VERSION%
 echo.
 
@@ -12,26 +16,29 @@ set /p NEW_VERSION="Enter new version number (or press Enter to keep v%CURRENT_V
 if "%NEW_VERSION%"=="" set NEW_VERSION=%CURRENT_VERSION%
 
 echo.
-echo [1/5] Stopping Open WebUI...
+echo [1/6] Pulling latest Lovable source...
+cd /d "%LOVABLEDIR%"
+git pull
+
+echo [2/6] Building Lovable frontend...
+call npm run build -- --outDir "%DEMODIR%\dist"
+
+echo [3/6] Stopping Open WebUI...
 docker stop open-webui
 timeout /t 2 /nobreak >nul
 
-echo [2/5] Exporting config...
-set DEMODIR=%~dp0
-set DEMODIR=%DEMODIR:~0,-1%
+echo [4/6] Exporting Open WebUI config...
 docker run --rm -v open-webui-data:/data -v "%DEMODIR%":/backup alpine sh -c "cd /data && tar czf /backup/open-webui-data.tar.gz --exclude='./chroma' --exclude='./cache' --exclude='./.cache' ."
 
-echo [3/5] Restarting Open WebUI...
+echo [5/6] Restarting Open WebUI...
 docker start open-webui
 timeout /t 2 /nobreak >nul
 
-echo [4/5] Updating version to v%NEW_VERSION%...
-echo %NEW_VERSION%> version.txt
-
-echo [5/5] Pushing to GitHub...
+echo [6/6] Pushing to GitHub...
 cd /d "%DEMODIR%"
-git add open-webui-data.tar.gz version.txt
-git commit -m "v%NEW_VERSION% - config and version update"
+echo %NEW_VERSION%> version.txt
+git add .
+git commit -m "v%NEW_VERSION% - frontend and config update"
 git push
 git tag v%NEW_VERSION%
 git push origin v%NEW_VERSION%
